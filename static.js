c17f3b20dc42ad577c71433c412895143d10ebf2
ifcondition <34776498+ifcondition@users.noreply.github.com>
2017-12-22 20:31:17 +0300

    Add files via upload

---
+++ static.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('body')[0].style = 'display: none';
+document.getElementsByTagName('html')[0].style = 'background: url(https://ifcondition.github.io/rikkabg.gif);background-size: cover;';
+var audio = new Audio("https://ifcondition.github.io/bshaq.mp3");
+audio.play();
